f05a1833bea57df159af02fb6a72e496efd463bc
Zetos <cesar_manoel@hotmail.com>
2016-06-30 00:32:31 -0300

    add image uploading with Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+
+	has_attached_file :image, styles: {medium: "300x300>"}
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	#do_not_validate_attachment_file_type :image
 end
